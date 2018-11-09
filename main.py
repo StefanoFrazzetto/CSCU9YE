1bae5f3a5c4a3345fa97b687f2ff271d4aa0226d
Stefano Frazzetto <s.frazzetto22@gmail.com>
2018-11-09 03:16:56 +0000

    Testing progress with working GreedyConstructive

--- main.py
+++ main.py
@@ -1,55 +1,28 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import random
-import os
 
+import Utils
+from Algorithms import Algorithm, AlgorithmType
+from Colour import Colour, ColoursList
 
-# Reads the file  of colours
-# Returns the number of colours in the file and a list with the colours (RGB) values
+dir_path = Utils.get_current_dir()  # Get current dir
+Utils.change_dir(dir_path)  # Change the working directory so we can read the file
 
-def read_file(fname):
-    with open(fname, 'r') as afile:
-        lines = afile.readlines()
-    n = int(lines[3])  # number of colours  in the file
-    col = []
-    lines = lines[4:]  # colors as rgb values
-    for l in lines:
-        rgb = l.split()
-        col.append(rgb)
-    return n, col
+ncolors, colours = Utils.read_file('colours.txt')  # Total number of colours and list of colours
 
-
-# Display the colours in the order of the permutation in a pyplot window
-# Input, list of colours, and ordering  of colours.
-# They need to be of the same length
-
-def plot_colours(col, perm):
-    assert len(col) == len(perm)
-
-    ratio = 10  # ratio of line height/width, e.g. colour lines will have height 10 and width 1
-    img = np.zeros((ratio, len(col), 3))
-    for i in range(0, len(col)):
-        img[:, i, :] = colours[perm[i]]
-
-    fig, axes = plt.subplots(1, figsize=(8, 4))  # figsize=(width,height) handles window dimensions
-    axes.imshow(img, interpolation='nearest')
-    axes.axis('off')
-    plt.show()
-
-
-#####_______main_____######
-
-# Get the directory where the file is located
-dir_path = os.path.dirname(os.path.realpath(__file__))
-os.chdir(dir_path)  # Change the working directory so we can read the file
-
-ncolors, colours = read_file('colours.txt')  # Total number of colours and list of colours
-
-test_size = 100  # Size of the subset of colours for testing
+test_size = 200  # Size of the subset of colours for testing
 test_colours = colours[0:test_size]  # list of colours for testing
 
-permutation = random.sample(range(test_size),
-                            test_size)  # produces random pemutation of lenght test_size, from the numbers 0 to test_size -1
-plot_colours(test_colours, permutation)
+colours_list = ColoursList.list_from_tuple_list(test_colours)
+
+# perm = random.sample(range(test_size), test_size)
+# # produces random pemutation of lenght test_size, from the numbers 0 to test_size -1
+# Utils.plot_colours(test_colours, perm)
 
 
+cl = colours_list.random_permutation(test_size)
+Utils.plot_colours_improved(cl)
+
+algorithm = Algorithm.factory(AlgorithmType.GREEDY_CONSTRUCTIVE)
+algorithm.load_colours_list(cl)
+cl_s = algorithm.get_solution(200)
+Utils.plot_colours_improved(cl_s)
